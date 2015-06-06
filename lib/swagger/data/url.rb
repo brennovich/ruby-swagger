e5f91cb17c4e7535d9d35d91fb8712d2ee4902a1
Luca Bonmassar <luca.bonmassar@gmail.com>
2015-06-06 13:43:30 -0700

    swagger data type URL

---
+++ lib/swagger/data/url.rb
@@ -0,0 +1,22 @@
+require 'addressable/uri'
+
+module Swagger::Data
+  class Url
+
+    SCHEMES = %w(http https)
+
+    attr_reader :url
+
+    def initialize(url)
+      @url = url
+    end
+
+    def valid?
+      parsed = Addressable::URI.parse(url) or return false
+      SCHEMES.include?(parsed.scheme)
+    rescue Addressable::URI::InvalidURIError
+      false
+    end
+
+  end
+end
