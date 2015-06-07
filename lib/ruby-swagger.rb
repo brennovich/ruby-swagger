c25baacc8322b04d08e2d9c843ccfe47889094e9
Luca Bonmassar <luca.bonmassar@gmail.com>
2015-06-06 19:01:53 -0700

    move main lib

---
+++ lib/ruby-swagger.rb
@@ -0,0 +1,6 @@
+require 'ruby-swagger/document'
+require 'ruby-swagger/info'
+require 'ruby-swagger/contact'
+require 'ruby-swagger/license'
+
+require 'ruby-swagger/railtie' if defined?(Rails)
--- lib/swagger.rb
+++
@@ -1,4 +0,0 @@
-require 'swagger/document'
-require 'swagger/info'
-require 'swagger/contact'
-require 'swagger/license'
