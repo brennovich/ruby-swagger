fd925bfcbc5fc651f2355f1d0533e7469344acb7
Luca Bonmassar <luca.bonmassar@gmail.com>
2015-06-07 14:42:24 -0700

    test for serialization / compile / deserialization

--- spec/swagger/io/file_system_spec.rb
+++ spec/swagger/io/file_system_spec.rb
@@ -91,7 +91,41 @@ describe Swagger::IO::FileSystem do
   end
 
   context 'when compiling data' do
-    #NOT IMPLEMENTED
+    before do
+      #stubbing the base_doc
+      content = File.open("#{File.dirname(__FILE__)}/../../fixtures/yaml/base_doc.yaml", 'r').read
+      FileUtils.mkdir_p(DOC_PATH);
+      File.open("#{DOC_PATH}/base_doc.yaml", 'w'){|f| f.write(content)}
+    end
+
+    after do
+      FileUtils.rm_rf(DOC_PATH) if Dir.exists?(DOC_PATH) #cleanup
+    end
+
+    it 'should generate a valid swagger document' do
+      Swagger::IO::FileSystem.new(Swagger::IO::FileSystem.read).compile!
+
+      doc = Swagger::Document.parse(JSON.parse(File.open("#{DOC_PATH}/swagger.json", 'r').read))
+
+      expect(doc).not_to be_nil
+      expect(doc.swagger).to eq "2.0"
+      expect(doc.schemes).to eq ['http']
+      expect(doc.consumes).to eq ['application/json']
+      expect(doc.produces).to eq ['application/json']
+
+      expect(doc.host).to eq 'petstore.swagger.io'
+      expect(doc.basePath).to eq '/api'
+
+      expect(doc.info.version).to eq "1.0.0"
+      expect(doc.info.title).to eq "Swagger Petstore"
+      expect(doc.info.description).to eq "A sample API that uses a petstore as an example to demonstrate features in the swagger-2.0 specification"
+      expect(doc.info.termsOfService).to eq "http://swagger.io/terms/"
+      expect(doc.info.contact.name).to eq "Swagger API Team"
+      expect(doc.info.contact.email).to eq "apiteam@swagger.io"
+      expect(doc.info.contact.url).to eq "http://swagger.io"
+      expect(doc.info.license.name).to eq "MIT"
+      expect(doc.info.license.url).to eq "http://github.com/gruntjs/grunt/blob/master/LICENSE-MIT"
+    end
   end
 
 end
