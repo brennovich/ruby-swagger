452a8d5f59e91e14523b4595c523ffec381e4d71
Luca Bonmassar <luca.bonmassar@gmail.com>
2015-06-10 16:54:29 -0700

    xml object spec
    https://github.com/swagger-api/swagger-spec/blob/master/versions/2.0.md#
    xmlObject

---
+++ lib/ruby-swagger/data/xml_object.rb
@@ -0,0 +1,15 @@
+require 'ruby-swagger/object'
+
+module Swagger::Data
+  class XMLObject < Swagger::Object #https://github.com/swagger-api/swagger-spec/blob/master/versions/2.0.md#xmlObject
+
+    attr_swagger :name, :namespace, :prefix, :attribute, :wrapped
+
+    def self.parse(xml_object)
+      return nil unless xml_object
+
+      Swagger::Data::XMLObject.new.bulk_set(xml_object)
+    end
+
+  end
+end
