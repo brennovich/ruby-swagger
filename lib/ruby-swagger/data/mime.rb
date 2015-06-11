78fbc7b29ebec7e5e725d7fe2b07d87a24bf55fb
Luca Bonmassar <luca.bonmassar@gmail.com>
2015-06-11 14:55:54 -0700

    update data types

--- lib/ruby-swagger/data/mime.rb
+++ lib/ruby-swagger/data/mime.rb
@@ -1,18 +1,27 @@
 module Swagger::Data
   class Mime
 
-    @@types = %w(
-      text/plain; charset=utf-8
-      application/json
-      application/vnd.github+json
-      application/vnd.github.v3+json
-      application/vnd.github.v3.raw+json
-      application/vnd.github.v3.text+json
-      application/vnd.github.v3.html+json
-      application/vnd.github.v3.full+json
-      application/vnd.github.v3.diff
-      application/vnd.github.v3.patch'
-    )
+    @@types = [
+      'text/plain',
+      'text/plain; charset=utf-8',
+      'application/octet-stream',
+      'application/json',
+      'application/postscript',
+      'application/pdf',
+      'application/postscript',
+      'application/octet-stream',
+      'application/xml',
+      'application/zip',
+      'text/xml',
+      'text/tab-separated-values',
+      'application/vnd.github+json',
+      'application/vnd.github.v3+json',
+      'application/vnd.github.v3.raw+json',
+      'application/vnd.github.v3.text+json',
+      'application/vnd.github.v3.html+json',
+      'application/vnd.github.v3.full+json',
+      'application/vnd.github.v3.diff',
+      'application/vnd.github.v3.patch']
 
     def self.valid?(type)
       @@types.include?(type)
