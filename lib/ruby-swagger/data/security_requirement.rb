eb522f15d95bd855f018b1ccc71454042bbf7cbb
Luca Bonmassar <luca.bonmassar@gmail.com>
2015-06-16 18:08:56 -0700

    fix invalid _desc method

--- lib/ruby-swagger/data/security_requirement.rb
+++ lib/ruby-swagger/data/security_requirement.rb
@@ -16,9 +16,9 @@ module Swagger::Data
     end
 
     def add_requirement(key, requirements)
-      raise (ArgumentError.new("key is nil [#{Swagger::Data::SecurityRequirement._desc}]")) unless key
-      raise (ArgumentError.new("requirements is nil [#{Swagger::Data::SecurityRequirement._desc}]")) unless requirements
-      raise (ArgumentError.new("requirements is not an array [#{Swagger::Data::SecurityRequirement._desc}]")) unless requirements.is_a?(Array)
+      raise (ArgumentError.new("Swagger::Data::SecurityRequirement#add_requirement - key is nil")) unless key
+      raise (ArgumentError.new("Swagger::Data::SecurityRequirement#add_requirement - requirements is nil")) unless requirements
+      raise (ArgumentError.new("Swagger::Data::SecurityRequirement#add_requirement - requirements is not an array")) unless requirements.is_a?(Array)
 
       @requirements[key] = requirements
     end
