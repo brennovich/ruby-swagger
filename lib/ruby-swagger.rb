b8ff86c6feb3ba2f12a465c139d595958bec74b6
Luca Bonmassar <luca.bonmassar@gmail.com>
2015-06-20 11:08:03 -0700

    include grape extension if grape is present

--- lib/ruby-swagger.rb
+++ lib/ruby-swagger.rb
@@ -1,3 +1,8 @@
 require 'ruby-swagger/data/document'
 
 require 'ruby-swagger/railtie' if defined?(Rails)
+
+if defined?(Grape)
+  require 'ruby-swagger/grape/grape_presenter'
+  require 'ruby-swagger/grape/grape-ext'
+end
