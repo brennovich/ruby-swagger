75cf655d7bf9f266ba07b07ad0240a347ce39c6f
Luca Bonmassar <luca.bonmassar@gmail.com>
2015-07-06 16:40:13 -0700

    refactor type and param

---
+++ lib/ruby-swagger/grape/param.rb
@@ -0,0 +1,34 @@
+require 'ruby-swagger/grape/type'
+
+module Swagger::Grape
+  class Param
+
+    def initialize(param)
+      @param = param
+    end
+
+    def to_swagger
+      swagger_param = {}
+      swagger_param['description'] = @param[:desc]  if @param[:desc].present?
+      swagger_param['default'] = @param[:default]   if @param[:default].present?
+      swagger_param['required'] = @param[:required] if @param[:required].present?
+
+      swagger_param.merge! Swagger::Grape::Type.new(@param[:type]).to_swagger
+
+      swagger_param
+    end
+
+    def has_type_definition?
+      type.downcase == 'object'
+    end
+
+    def type_definition
+      (Object.const_get(type)).to_s
+    end
+
+    def type
+      @param[:type].to_s || 'string'
+    end
+
+  end
+end
--- lib/ruby-swagger/grape/type.rb
+++ lib/ruby-swagger/grape/type.rb
@@ -2,13 +2,44 @@ module Swagger::Grape
   class Type
 
     def initialize(type)
-      @type = type
+      @type = type.to_s || 'String'
     end
 
-    def to_swagger
-      {"moo"=>"miao"}
-    end
+    def to_swagger(with_definition = true)
+      swagger_type = {}
 
+      case @type.downcase
+        when 'string'
+          swagger_type['type'] = 'string'
+        when 'integer'
+          swagger_type['type'] = 'integer'
+        when 'array'
+          swagger_type['type'] = 'array'
+          swagger_type['items'] = {'type' => 'string'}
+        when 'hash'
+          swagger_type['type'] = 'object'
+          swagger_type['properties'] = {}
+        when 'virtus::attribute::boolean'
+          swagger_type['type'] = 'boolean'
+        when 'symbol'
+          swagger_type['type'] = 'string'
+        when 'float'
+          swagger_type['type'] = 'number'
+          swagger_type['format'] = 'float'
+        when 'rack::multipart::uploadedfile'
+          swagger_type['type'] = 'file'
+        when 'date'
+          swagger_type['type'] = 'date'
+        when 'datetime'
+          swagger_type['format'] = 'date-time'
+          swagger_type['format'] = 'string'
+        else
+          swagger_type['type'] = "object"
+          swagger_type['schema'] = {"$ref" => "#/definitions/#{@type}"}
+      end
+
+      swagger_type
+    end
 
   end
 end
