30740235ad4e0c519357b7a50d62c4fef774e000
Luca Bonmassar <luca.bonmassar@gmail.com>
2015-07-07 09:34:23 -0700

    extract all types in the definition

--- lib/ruby-swagger/grape/grape_template.rb
+++ lib/ruby-swagger/grape/grape_template.rb
@@ -15,13 +15,34 @@ module Swagger::Grape
       swagger_doc.paths = routes.to_swagger
       swagger_doc.definitions = Swagger::Data::Definitions.new
 
-      routes.types.sort.each do |type|
+      extract_all_types(routes.types).sort.each do |type|
         grape_type = Swagger::Grape::Type.new(type)
 
         swagger_doc.definitions.add_definition(type.to_s, grape_type.to_swagger(false))
       end
 
       swagger_doc
+    end
+
+    def self.extract_all_types(types, all_types = [])
+      return all_types.uniq if types.length == 0
+
+      new_types = []
+
+      types.each do |type|
+        all_types << type.to_s unless all_types.include?(type.to_s)
+
+        grape_type = Swagger::Grape::Type.new(type)
+
+        grape_type.sub_types.each do |new_type|
+          unless all_types.include?(new_type.to_s)
+            new_types << new_type.to_s
+            all_types << new_type.to_s
+          end
+        end
+      end
+
+      extract_all_types(new_types, all_types)
     end
 
   end
