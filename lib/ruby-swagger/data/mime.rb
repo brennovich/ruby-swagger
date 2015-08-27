1e1e71064d36553bd59ca494786636530195d58e
Dan Cutting <dan@cutting.io>
2015-08-27 11:18:40 +0100

    Permit any MIME type.

--- lib/ruby-swagger/data/mime.rb
+++ lib/ruby-swagger/data/mime.rb
@@ -1,32 +1,8 @@
 module Swagger::Data
   class Mime
 
-    @@types = [
-      'text/plain',
-      'text/plain; charset=utf-8',
-      'application/octet-stream',
-      'application/json',
-      'application/hal+json',
-      'application/problem+json',
-      'application/postscript',
-      'application/pdf',
-      'application/postscript',
-      'application/octet-stream',
-      'application/xml',
-      'application/zip',
-      'text/xml',
-      'text/tab-separated-values',
-      'application/vnd.github+json',
-      'application/vnd.github.v3+json',
-      'application/vnd.github.v3.raw+json',
-      'application/vnd.github.v3.text+json',
-      'application/vnd.github.v3.html+json',
-      'application/vnd.github.v3.full+json',
-      'application/vnd.github.v3.diff',
-      'application/vnd.github.v3.patch']
-
     def self.valid?(type)
-      @@types.include?(type)
+      true
     end
 
   end
